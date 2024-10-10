0b2e008d6847642cb42349dfa39e6ae8a82a5021
Felix Rong <felix.rong@grow.inc>
2024-10-11 10:17:11 +1100

    Corrected the jwt issuer

--- api/service/src/main/java/au/com/rainmore/centus/services/core/security/jwt/SecurityConstants.java
+++ api/service/src/main/java/au/com/rainmore/centus/services/core/security/jwt/SecurityConstants.java
@@ -6,7 +6,7 @@ public class SecurityConstants {
     public static final String AUTHORITIES               = "Authorities";
     public static final String TOKEN_PREFIX              = "Bearer ";
     public static final String JWT_TOKEN_HEADER          = "Jwt-Token";
-    public static final String GET_ARRAYS_LLC            = "Get Arrays, LLC";
+    public static final String GET_ARRAYS_LLC            = "Rainmore";
     public static final String GET_ARRAYS_ADMINISTRATION = "Spring React Demo";
     public static final String FORBIDDEN_MESSAGE         = "Please login";
 
